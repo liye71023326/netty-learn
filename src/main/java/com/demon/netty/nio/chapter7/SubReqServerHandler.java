8fcfe38d14ae51514cc756341c30748cfee47a58
demon <liye71023326@163.com>
2014-08-19 00:06:33 +0800

    netty java serial

---
+++ src/main/java/com/demon/netty/nio/chapter7/SubReqServerHandler.java
@@ -0,0 +1,33 @@
+package com.demon.netty.nio.chapter7;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by demon
+ * Date: 14-8-18 下午11:30.
+ */
+public class SubReqServerHandler extends ChannelHandlerAdapter{
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        SubscribeReq req = (SubscribeReq) msg;
+        if ("LiYe".equalsIgnoreCase(req.getUserName())){
+            System.out.println("Service accept client subscribe req : [" + req.toString() + "]");
+            ctx.writeAndFlush(resp(req.getSubReqID()));
+        }
+    }
+
+    private SubscribeResp resp(int subReqID){
+        SubscribeResp resp = new SubscribeResp();
+        resp.setSubReqID(subReqID);
+        resp.setRespCode(0);
+        resp.setDesc("Netty book oder succeed, 3 days later, sent to the designated address");
+        return resp;
+    }
+}
---
+++ src/main/java/com/demon/netty/nio/chapter7/SubscribeResp.java
@@ -0,0 +1,49 @@
+package com.demon.netty.nio.chapter7;
+
+import java.io.Serializable;
+
+/**
+ * Created by demon
+ * Date: 14-8-18 下午11:12.
+ */
+public class SubscribeResp implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private int subReqID;
+    private int respCode;
+    private String desc;
+
+    public int getSubReqID() {
+        return subReqID;
+    }
+
+    public void setSubReqID(int subReqID) {
+        this.subReqID = subReqID;
+    }
+
+    public int getRespCode() {
+        return respCode;
+    }
+
+    public void setRespCode(int respCode) {
+        this.respCode = respCode;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        return "SubscribeResp{" +
+                "subReqID=" + subReqID +
+                ", respCode=" + respCode +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+}
