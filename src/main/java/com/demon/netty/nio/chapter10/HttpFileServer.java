9a23c1bd0b6f2b652d3e266f63ad5976a9785d5f
demon <liye71023326@163.com>
2014-08-21 00:50:26 +0800

    http 文件服务器

---
+++ src/main/java/com/demon/netty/nio/chapter10/HttpFileServer.java
@@ -0,0 +1,62 @@
+package com.demon.netty.nio.chapter10;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * Created by demon
+ * Date: 14-8-19 下午11:17.
+ */
+public class HttpFileServer {
+
+    private static final String DEFAULT_URL = "/src/main/java/com/demon/netty/";
+
+    public void run(final int port, final String url) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                            ch.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                            ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                            ch.pipeline().addLast("fileServerHandler", new HttpFileServerHandler(url));
+                        }
+                    });
+
+            ChannelFuture channelFuture = bootstrap.bind("192.168.1.104", port).sync();
+            System.out.println("Http 文件目录服务器启动， 网址 ：" + "192.168.1.104:" + port + url);
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0){
+            port = Integer.valueOf(args[0]);
+        }
+        String url = DEFAULT_URL;
+        if ( args != null && args.length > 1) {
+            url = args[1];
+        }
+        new HttpFileServer().run(port, url);
+    }
+}
